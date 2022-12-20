73fd1badc0ca7e8feb3fd8107bf72fcc5c0bcf0c
kartikmehta056 <120786943+kartikmehta056@users.noreply.github.com>
2022-12-20 08:59:32 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,11 +2,11 @@
 
 cd /root
 rm -rf docker ;
-git clone https://github.com/mycloudazure/docker.git ;
+git clone https://github.com/kartikmehta056/docker ;
 docker system prune -af ;
 cd /root/docker ;
-docker build -t myawspk/besant22:myapache .
-docker push myawspk/besant22:myapache
-docker rm -f mycontianer
+docker build -t kartikmehta056/dockerfile:myimg .
+docker push kartikmehta056/dockerfile:myimg
+docker rm -f mycontainer
 docker system prune -af
-docker run -itd -p 80:80 --name mycontianer myawspk/besant22:myapache
+docker run -itd -p 80:80 --name mycontianer kartikmehta056/dockerfile:myimg
